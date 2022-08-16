a5d13ebe660ddc2f170adc932121fb75a685fa5b
Rydwan94 <rydwan94@gmail.com>
2022-08-16 20:50:27 +0200

    added projects container

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
-function fs(a, b, c) {
-  return a + b + c;
-}
+
+const mainSiteContainer = document.getElementById("mainSiteContainer");
+const myProjectsContainer = document.getElementById("myProjectsContainer");
+const showMainSite = document.getElementById("showMainSite");
+
+showProjects.addEventListener("click", () => {
+  mainSiteContainer.style.display = "none";
+  myProjectsContainer.style.display = "block";
+});
+
+showMainSite.addEventListener("click", () => {
+  myProjectsContainer.style.display = "none"
+  mainSiteContainer.style.display = "block"
+});
+
+
