1fc47953afea214e3f75b5e03205e95370fa6558
Rydwan94 <rydwan94@gmail.com>
2022-08-22 11:27:55 +0200

    Added form

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,28 @@
 
 const mainSiteContainer = document.getElementById("mainSiteContainer");
 const myProjectsContainer = document.getElementById("myProjectsContainer");
+const formContainer = document.getElementById("formContainer")
 const showMainSite = document.getElementById("showMainSite");
+const navBar = document.getElementById("navBar")
+
+
+const showForm = (id) => {
+  id.addEventListener("click", () =>{
+    myProjectsContainer.style.display = "none";
+      navBar.style.display = "none"
+      formContainer.style.display = "block"
+  })
+}
+
+showForm(firsProjectButton)
+showForm(secondProjectButton)
+showForm(thirdProjectButton)
+showForm(fourthProjectButton)
+
+
+myProjectsContainer.style.display = "none"
+formContainer.style.display = "none"
+
 
 showProjects.addEventListener("click", () => {
   mainSiteContainer.style.display = "none";
@@ -13,4 +34,16 @@ showMainSite.addEventListener("click", () => {
   mainSiteContainer.style.display = "block"
 });
 
+
+class Projects{
+  constructor(id,projectName, img, price){
+    this.id = document.getElementById(id),
+    this.projectName = projectName,
+    this.img = img,
+    this.price = price
+  }
+
+}
+
+let modernProject = new Projects("firsProjectButton", "Modern Style", "sdad", 200)
 
